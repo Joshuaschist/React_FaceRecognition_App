c4fa94c61b859a56e9cccd5ea5e0dfd9a237cdc3
Joshua Ezem <chisomjoshua37@gmail.com>
2020-11-08 17:06:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   const width = Number(image.width);
   const height = Number(image.height);
   return {
-    leftCol: clarifaiFace.left_Col * width,
+    leftCol: clarifaiFace.left_col * width,
     topRow: clarifaiFace.top_row * height,
     rightCol: width - (clarifaiFace.right_col * width),
     bottomRow: height - (clarifaiFace.bottom_row * width)
